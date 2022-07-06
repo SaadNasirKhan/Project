895b8f420a1c2ba5496d703e5410b26538dd70e1
SaadNasirKhan <108619291+SaadNasirKhan@users.noreply.github.com>
2022-07-06 00:31:38 -0400

    Add files via upload

---
+++ Q2.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Q2
+{
+
+    class SalarySlip
+    {
+        protected double baseSal = 1500;
+        protected int medical;
+        public string designation;
+        protected int fuel;
+        public double total;
+        public int i;
+        public int j;
+
+
+
+
+        public string Designation
+        {
+            get { return designation; }
+            set { designation = value; }
+        }
+
+
+        public int Medical
+        {
+            get { return medical; }
+            set { medical = value; }
+        }
+
+        public int Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+
+        public void Total(int medical, int fuel)
+        {
+            total = medical + fuel + baseSal;
+        }
+
+    }
+
+    // Derived class
+    class Engineer : SalarySlip
+    {
+
+        public double grossengineer()
+        {
+            //medical = 500;
+            //fuel=100;
+
+            Console.WriteLine("Write medical allowance : ");
+            i = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Write fuel allowance: ");
+            j = Convert.ToInt32(Console.ReadLine());
+
+            this.Total(i, j);
+            Console.WriteLine("Gross Salary of an Engineer is: " + total);
+            return total;
+        }
+    }
+
+    class Manager : SalarySlip
+    {
+
+        public double grossmanager()
+        {
+            //medical = 1000;
+            //fuel = 250;
+
+            Console.WriteLine("Write medical allowance : ");
+            i = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Write fuel allowance: ");
+            j = Convert.ToInt32(Console.ReadLine());
+
+            this.Total(i, j);
+            Console.WriteLine("Gross Salary of an Manager is: " + total);
+
+            return 0;
+        }
+    }
+
+    class Analyst : SalarySlip
+    {
+
+        public double grossanalyst()
+        {
+            //medical = 800;
+            //fuel = 150;
+
+            Console.WriteLine("Write medical allowance : ");
+            i = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Write fuel allowance: ");
+            j = Convert.ToInt32(Console.ReadLine());
+
+            this.Total(i, j);
+            Console.WriteLine("Gross Salary of an Analyst is: " + total);
+
+            return total;
+        }
+    }
+
+    class Tester
+    {
+
+        static void Main(string[] args)
+        {
+            Engineer e = new Engineer();
+            Manager m = new Manager();
+            Analyst a = new Analyst();
+            SalarySlip s = new SalarySlip();
+
+            Console.WriteLine("Type your designation: ");
+            s.designation = Console.ReadLine();
+
+            if (s.designation == "Engineer")
+            {
+                e.grossengineer();
+            }
+            else if (s.designation == "Manager")
+            {
+                m.grossmanager();
+            }
+            else if (s.designation == "Analyst")
+            {
+                a.grossanalyst();
+            }
+        }
+    }
+}
---
+++ Q3.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Q3
+{
+
+    class ElectricBill
+    {
+        public double units;
+        public string TypeBill;
+
+        //public double unit
+        //{
+         //   get { return units; }
+          //  set { units = value; }
+        //}
+
+        public string Type
+        {
+            get { return TypeBill; }
+            set { TypeBill = value; }
+        }
+
+    }
+
+    // Derived class
+    class Resedential : ElectricBill
+    {
+        public double unit
+        {
+            get { return units; }
+            set { units = value; }
+        }
+        public double ResidentialBill()
+        {
+            unit = units;
+            if (unit <= 100)
+            {
+                return unit * 5;
+            }
+
+            else if (unit > 100 && unit <= 200)
+            {
+                return (100 * 5) +
+               (unit - 100) * 17;
+            }
+
+            else if (unit > 200 && unit <= 500)
+            {
+                return (100 * 5) +
+               (17 * 100) +
+               (unit - 300) * 23;
+            }
+
+            else if (unit > 500)
+            {
+                return (100 * 5) +
+               (17 * 100) +
+               (23 * 400) +
+               (unit - 500) * 69;
+            }
+
+            return 0;
+        }
+
+
+    }
+
+    class Commercial : ElectricBill
+    {
+        public double unit
+        {
+            get { return units; }
+            set { units = value; }
+        }
+        public double CommercialBill()
+        {
+            unit = units;
+            if (unit <= 100)
+            {
+                return unit * 8;
+            }
+
+            else if (unit > 100 && unit <= 200)
+            {
+                return (100 * 8) +
+               (unit - 100) * 21;
+            }
+
+            else if (unit > 200 && unit <= 500)
+            {
+                return (100 * 5) +
+               (17 * 100) +
+               (unit - 300) * 23;
+            }
+
+            else if (unit > 500)
+            {
+                return (100 * 5) +
+               (17 * 100) +
+               (23 * 400) +
+               (unit - 500) * 79;
+            }
+
+            return 0;
+        }
+    }
+
+
+
+    class Tester
+    {
+
+        static void Main(string[] args)
+        {
+
+            ElectricBill e = new ElectricBill();
+            Resedential r = new Resedential();
+            Commercial c = new Commercial();
+            Console.WriteLine("Are you Resedential or Commercial: ");
+            e.Type = Console.ReadLine();
+
+
+            
+
+            if (e.Type == "Resedential")
+            {
+                Console.WriteLine("Insert unit: ");
+                r.units = Convert.ToInt32(Console.ReadLine());
+                r.ResidentialBill();
+                Console.WriteLine("Residential Bill is " + r.ResidentialBill());
+                double newresidential = r.ResidentialBill() + r.ResidentialBill() * 0.13;
+                Console.WriteLine("New Residential Bill is " + newresidential);
+
+            }
+            else if (e.Type == "Commercial")
+            {
+                Console.WriteLine("Insert unit: ");
+                c.units = Convert.ToInt32(Console.ReadLine());
+                c.CommercialBill();
+                Console.WriteLine("Residential Bill is " + c.CommercialBill());
+                double newcommercial = c.CommercialBill() + c.CommercialBill() * 0.17;
+                Console.WriteLine("New Residential Bill is " + newcommercial);
+            }
+
+        }
+    }
+}
